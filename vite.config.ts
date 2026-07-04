f75bcb68aeaaca1af4d58fe34e91d92390e64988
RonielSabala <ronielsabala11@gmail.com>
2026-07-03 21:37:27 -0400

    chore: clean project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
